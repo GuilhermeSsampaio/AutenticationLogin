a085cb01aa03febcc3ba9424bb90dd7ffc75db15
GuilhermesSampaio <guilhermessampaio20@gmail.com>
2022-10-08 16:39:16 -0400

    resetando projeto

--- app/routes/login.js
+++
@@ -1,9 +0,0 @@
-// module.exports = function(app, req, res) {
-// app.get('/login', () => {
-//     res.render(("login", {title: 'teste'}));
-// })
-// app.post('/login', () => {
-	
-// }
-// )
-// }
