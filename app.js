1df22786ef5ead066f2b5866e9a09c025a506b43
GuilhermesSampaio <guilhermessampaio20@gmail.com>
2022-10-07 22:13:42 -0400

    projeto funcionando // falta register

--- app.js
+++ app.js
@@ -11,9 +11,23 @@ const {
 } = process.env
 
 const IN_PROD = NODE_ENV === 'production'
-
-const users = [
-    {id: 1, name: 'Alex', email: 'alex@gmail.com', password: 'secret'}, {id: 2, name: 'Gui', email: 'gui@gmail.com', password: 'secret'}, {id: 1, name: 'Pedro', email: 'pedro@gmail.com', password: 'secret'}]
+//aqui usario o db
+const users = [{
+    id: 1,
+    name: 'Alex',
+    email: 'alex@gmail.com',
+    password: 'secret'
+}, {
+    id: 2,
+    name: 'Gui',
+    email: 'gui@gmail.com',
+    password: 'secret'
+}, {
+    id: 1,
+    name: 'Pedro',
+    email: 'pedro@gmail.com',
+    password: 'secret'
+}]
 
 const app = express()
 app.use(bodyParser.urlencoded({
@@ -48,8 +62,15 @@ const redirectHome = (req, res, next) => {
     }
 }
 
+app.use((req, res, next) => {
+    const {userID} = req.session
+    if(userID){
+        res.locals.user = users.find(user => user.id === userID)
+    }
+    next()
+})
 
-app.get('/', redirectLogin, (req, res) => {
+app.get('/', (req, res) => {
     const {
         userID
     } = req.session
@@ -66,6 +87,25 @@ app.get('/', redirectLogin, (req, res) => {
         `}  
     `)
 })
+
+
+app.get('/home', redirectLogin, (req, res) => {
+    const {user} = res.locals
+console.log(req.session.userID)
+    res.send(`
+        <h1>home</h1>   
+        <a href= '/'> menu </a>
+        <ul>
+        <li>name: ${user.name} </li>
+        <li>email: ${user.email}</li>
+        </ul>
+    `)            
+})
+
+app.get('/profile', (req, res) => {
+    const {user} = res.locals
+})
+
 app.get('/login', (req, res) => {
     res.send(`
         <h1>Login</h1>
@@ -91,20 +131,57 @@ app.get('/register', (req, res) => {
 
     `)
 })
-app.post('/login', redirectHome, (req, res) => {
-    const {email,password} = req.body
-
-    if(email && password){
-        const user = user.find(
+app.post('/login', (req, res) => {
+    const {email, password} = req.body
+    // const user= "";
+    if (email && password) {
+        const user = users.find(
             user => user.email === email && user.password === password
         )
+        if (user) {
+            req.session.userID = user.id
+            return res.redirect('/home')
+        }
     }
+
+    // res.redirect('/login')
 })
 app.post('/register', redirectHome, (req, res) => {
+    const {
+        name,
+        email,
+        password
+    } = req.body
 
+    if (name && email && password) {
+        const exists = users.some(
+            user => user.email === email
+        )
+
+        if (!exists) {
+            const user = {
+                id: users.length + 1,
+                name,
+                email,
+                password
+            }
+        users.push(user)
+
+        req.session.userID = user.id
+
+        return res.redirect('/home')
+        }
+    }
+    res.redirect('/login')
 })
 
 app.post('/logout', redirectLogin, (req, res) => {
-
+    req.session.destroy(err => {
+        if(err){
+            return res.redirect('/home')
+        }
+        res.clearCookie(SESS_NAME)
+        res.redirect('/login')
+    })
 })
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
